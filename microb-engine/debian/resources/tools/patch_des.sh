a59f104d0eec799439b7c45e747b6638d9b96339
romaxa_xz <romaxa_xz@088d1160-0321-0410-a43d-9b691b16afed>
2007-08-03 11:25:05 +0000

    Added recursive checking for all diffs in directory ;)
    git-svn-id: https://vcs.maemo.org/svn/browser/mozilla/trunk@353 088d1160-0321-0410-a43d-9b691b16afed

--- microb-engine/debian/resources/tools/patch_des.sh
+++ microb-engine/debian/resources/tools/patch_des.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo '<html><table border="2">'
-for str in `ls *.diff`; do echo '<tr><td>' $str '</td>'; echo '<td>'; cat $str| sed -n '/^#/p'| sed 's/#/<br>/g'; echo '</td></tr>'; done
+for str in `find ./ -name "*.diff"`; do echo '<tr><td>' $str '</td>'; echo '<td>'; cat $str| sed -n '/^#/p'| sed 's/#/<br>/g'; echo '</td></tr>'; done
 echo '</table></html>'
 
