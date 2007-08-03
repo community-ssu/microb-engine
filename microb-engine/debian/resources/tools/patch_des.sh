6f2954407bf7cd2ca697f87228c079bed44df6d2
romaxa_xz <romaxa_xz@088d1160-0321-0410-a43d-9b691b16afed>
2007-08-03 11:39:57 +0000

    Improved patch descriptions extract tool for more simple using cd microb-engine/debian/patches ../resources/tools/patch_des.sh series.thebestrunk  > patches.html
    git-svn-id: https://vcs.maemo.org/svn/browser/mozilla/trunk@354 088d1160-0321-0410-a43d-9b691b16afed

--- microb-engine/debian/resources/tools/patch_des.sh
+++ microb-engine/debian/resources/tools/patch_des.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+
+mkdir ./temp_patches
+for str in `cat $1`; do cp $str ./temp_patches/ 2>&1| > /dev/null; done
+cd ./temp_patches
+
 echo '<html><table border="2">'
 for str in `find ./ -name "*.diff"`; do echo '<tr><td>' $str '</td>'; echo '<td>'; cat $str| sed -n '/^#/p'| sed 's/#/<br>/g'; echo '</td></tr>'; done
 echo '</table></html>'
+cd ..
+rm -rf ./temp_patches
 
