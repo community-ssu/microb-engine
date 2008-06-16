4da90f9f56f1ae0133e939b52e2e3433da91a1bf
romaxa_xz <romaxa_xz@088d1160-0321-0410-a43d-9b691b16afed>
2008-06-16 07:28:09 +0000

    Updated to trunk 20080616
    git-svn-id: https://vcs.maemo.org/svn/browser/mozilla/trunk@2557 088d1160-0321-0410-a43d-9b691b16afed

--- tarballs/get_latest_hg_tarball.sh
+++ tarballs/get_latest_hg_tarball.sh
@@ -8,6 +8,30 @@ CURDIR=$(pwd)
 #OLDVER=$1
 OLDVER=$(cat curver)
 
+svn update
+
+default_hg="http://hg.mozilla.org/mozilla-central"
+
+HG_REPO=$HG_REPOSITORY
+HG_LREPO=$HG_LREPOSITORY
+if [ "$HG_REPO" == "" ]; then
+  HG_REPO=$default_hg
+fi
+
+if [ "$HG_LREPO" == "" ]; then
+  hg clone $HG_REPO mozilla
+  HG_LREPO=$(pwd)/mozilla
+fi
+
+hg pull -R $HG_LREPO
+hg update -R $HG_LREPO
+CUR_REV=$(cat bonsai.date)
+LAST_REV=$(hg log --limit 1 -R $HG_LREPO | grep 'changeset' | sed 's/\:/\ /g' | awk '{print $3}')
+if [ "$CUR_REV" != "$LAST_REV" ];then
+echo "$LAST_REV" > bonsai.date
+fi
+echo $HG_REPO
+
 wget -c $HGURL -O temp.tar.gz
 rm -f mozilla.tar.gz
 mv temp.tar.gz mozilla.tar.gz
